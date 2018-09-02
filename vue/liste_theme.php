6d718f53781438371013b2f46733fef2d353344d
Davidjeje <david.marivat-larose@orange.fr>
2018-09-03 01:41:36 +0200

    adding the liste_theme.php file

---
+++ vue/liste_theme.php
@@ -0,0 +1,295 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Accueil </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+    
+    <link href="https://fonts.googleapis.com/css?family=Grand+Hotel" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans:100,200,300,400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="public/css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="public/css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="public/css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="public/css/magnific-popup.css">
+
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="public/css/flexslider.css">
+
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="public/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="public/css/owl.theme.default.min.css">
+    
+    <!-- Flaticons  -->
+    <link rel="stylesheet" href="public/fonts/flaticon/font/flaticon.css">
+
+    <!-- Theme style  -->
+    <link rel="stylesheet" href="public/css/style.css">
+
+    <!-- Modernizr JS -->
+    <script src="public/js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    </head>
+    <body>
+        
+    <div class="colorlib-loader"></div>
+
+    <div id="page">
+        <nav class="colorlib-nav" role="navigation">
+            <div class="top-menu">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-2">
+                            <div id="colorlib-logo">David Marivat</div>
+                        </div>
+                        <div class="col-xs-10 text-right menu-1">
+                            <ul>
+                                <li><a href="index.php">Accueil</a></li>
+                                <li class="has-dropdown"><a href="index.php?page=vers_liste_blog">Blog post</a></li>
+                                <li><a href="index.php?page=vers_connexion_user">Connexion/inscription</a></li>
+                                
+                            </ul>
+                                   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <aside id="colorlib-breadcrumbs">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 breadcrumbs text-center">
+                        <h2>Les 13 Tendances Qui Vont Bousculer Le Secteur De La Technologie En 2018</h2>
+                    </div>
+                </div>
+            </div>
+        </aside>
+                            
+                            
+                    
+                    
+
+        <div id="colorlib-container">
+            <div class="container">
+                <div class="row">
+                    <div class="form-group">
+                    <aside class="sidebar">
+                        
+                                     
+                    </aside>
+    
+                    <div class="col-md-6">
+                        <img class="img-responsive" src="public/images/avatar.png" alt="avatar david">
+                    </div>
+                    <div class="col-md-6">
+                        <p>La nouvelle année est arrivée, suivie de toute une panoplie de nouvelles tendances technologiques qu’il faudra surveiller de près. De la démocratisation de l’intelligence artificielle à l’augmentation de l’utilisation de la réalité virtuelle ou augmentée, ces technologies nous permettent déjà de gérer nos entreprises de manière plus efficace.</p>
+                        <blockquote>
+                            Que votre entreprise cherche à rester à la pointe de la technologie ou à protéger ses données en intégrant des technologies numériques avancés, les tendances technologiques de l’année vous aideront certainement. Selon un rapport publié par Deloitte, vous devriez être tout particulièrement attentifs aux avancées en matière d’intelligence artificielle, d’internet des objets et de réalité virtuelle.
+                        </blockquote>
+                        <p class="first-letra">Vous trouverez ci-dessous les tendances qui risquent de bouleverser les secteur de la technologie cette année selon 13 des membres du Forbes Technology Council.</p>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+        
+
+            <div id="colorlib-contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 animate-box">
+                        <p> <h2><strong>L’</strong><a href="https://www.forbes.fr/technologie/amazon-humilie-google-et-apple-dans-la-bataille-sur-lintelligences-artificielle/">intelligence artificielle</a> <strong>standardisée</strong></h2>
+
+                        Maintenant qu’il est possible de développer certaines des intelligences artificielles les plus avancées du marché en quelques heures sur son propre ordinateur avec un cadre en open source, l’IA va devenir omniprésente. Les logiciels seront plus intelligents et plus performants dans le traitement du langage naturel, de la vision informatique, des systèmes de recommandations et ils seront plus faciles à développer en CMS – <strong>Carson Kahn, Volley.com</strong>.
+                        </p>
+
+                        <p> <h2><strong>Les chatbots</strong></h2>
+
+                        Nous verrons davantage de chatbots et nous devrons apprendre à interagir avec. Nous pourrons ainsi leur apprendre ce que nous attendons d’eux – <strong>Chalmers Brown, Due</strong>.
+                        </p>
+
+                        <p> <h2><strong>La place grandissante de l’éthique dans le secteur de l’</strong><a href="https://www.forbes.fr/management/maitriser-sa-strategie-dinnovation-la-methode-acme/">innovation</a> <strong>technologique</strong></h2>
+
+                        Nous nous heurtons aux dilemmes éthiques qui sont liés directement à ce que nous créons et comment nous le créons : en orientant des publicités en fonction des facteurs démographiques, l’angoisse de l’automatisation des emplois, le revenu universel et même la fréquence du harcèlement sexuel. Le secteur de la tech évolue plus rapidement que les systèmes éthiques qui l’encadrent – Kieran Snyder, Textio.
+                        </p>
+
+                        <p> <h2><strong>La réalité augmentée</strong></h2>
+
+                        Snapchat a aidé à la démocratisation de la réalité augmentée grâce à ses filtres amusants. Plus récemment, <a href="https://www.youtube.com/watch?v=gZhQCVSvq5E">Apple</a> a présenté ARKit, permettant de faciliter l’intégration de la réalité augmentée dans les applications pour iPhones et des entreprises comme Amazon l’on déjà adopté. Je pense que 2018 sera sous le signe de la réalité augmentée – <strong>Thomas Griffin, OptinMonster</strong>.
+                        </p>
+
+                        <p> <h2><strong>Des fonctionnalités d’équipements informatiques et de logiciels compatibles avec la réalité augmentée et le machine learning</strong></h2>
+
+                        Bon nombre d’entreprises produisent des fonctionnalités d’équipements et des logiciels compatibles avec la réalité augmentée et le machine learning. Les développeurs savent tirer profit de ces puces et API pour créer des applications permettant de voir le monde différemment et de mieux communiquer avec les machines. Je prédis également que les nouvelles technologies seront utilisées pour contrôler la sécurité, aider à regrouper le big data et à améliorer les processus d’authentification – <strong>Chris Kirby, Voices.com</strong>.
+                        </p>
+
+                        <p><h2><strong>Une méthode d’analyse améliorée</strong></h2>
+
+                        L’analyse augmentée automatise les résultats des données à l’aide du machine learning et du langage naturel afin d’automatiser la préparation de données pour pouvoir ensuite les partager. Cette présentation des données aide à leur simplification dans le but d’obtenir un résultat clair tout en fournissant un accès à des outils sophistiqués qui confortent les clients et les employés avec le luxe de pouvoir prendre des décisions au jour le jour en toute confiance et objectivité – <strong>Jeremy Williams, Vyudu</strong>.
+                        </p>
+
+                        <p> <h2><strong>La pression pour migrer vers le cloud</strong></h2>
+
+                        Davantage d’entreprises migreront vers le cloud car davantage de PDG le demanderont, ce qui aura pour effet d’angoisser les équipes qui devront appliquer cette décision. Elles rencontreront beaucoup de difficultés et se demanderont si elles ont les compétences ou les équipes nécessaires pour le faire. Le cloud change la donne, mais engendre une certaine pression au sein des entreprises qui souhaitent s’y installer – <strong>Todd Delaughter, Automic Software</strong>.
+                        </p>
+
+                        <p> <h2><strong>Les piratages informatiques en constante augmentation</strong></h2>
+
+                        Les piratages seront plus nombreux que jamais, qu’ils soient ciblés ou non. Les attaquants viseront certainement des plateformes et des appareils qui sont mal protégés. Cette tendance est grandissante et elle coûtera de plus en plus cher à ses victimes – <strong>Ivailo Nikolov, SiteGround</strong>.
+                        </p>
+
+                        <p> <h2><strong>Les investissements dans le domaine de la cybersécurité</strong></h2>
+
+                        En 2017, les cyber attaques constantes ont bouleversé le secteur de la technologie. Il est aujourd’hui primordial d’investir dans le domaine de la cybersécurité. Cela m’intéresse tout particulièrement de voir comment la tech peut aider les secteurs privés et publics à améliorer leur protection. Le cloud propose des niveaux de sécurité plus importants, alors j’espère que les gens lui feront davantage confiance en 2018 –<strong> Marcus Turner, Enola Labs</strong>.
+                        </p>
+
+                        <p><h2><strong>La Blockchain</strong></h2>
+
+                        La technologie de la Blockchain doit être surveillée de près en 2018. La suppression des intermédiaires dans beaucoup de domaines affectera énormément les marchés. Ce système sans faille que nous promet la Blockchain devrait révolutionner le secteur de la finance. Nous assistons également à la naissance de tendances visant à améliorer le confort des consommateurs. Si elles sont bien conçues, ces solutions devraient réussir à tirer leur épingle du jeu – <strong>Jim Higgins, Solutionreach.com</strong>.
+                        </p>
+
+                        <p><h2><strong>L’<a href="https://www.forbes.fr/technologie/les-6-specialistes-de-lintelligence-artificielle-et-du-machine-learning-dans-le-monde/">intelligence artificielle</a> conversationnelle</strong></h2>
+
+                        L’<a href="https://www.forbes.fr/technologie/intelligence-artificielle-transhumanisme-nick-bostrom-futur-de-lhumanite/">intelligence artificielle</a> conversationnelle sera la seconde tendance la plus disruptive de 2018. Les applications conversationnelles dévoileront tout leur potentiel dans les secteurs des chaînes logistiques, de la vente, de la production et de l’assurance. Associées au succès des applications orientées vers le consommateur telles qu’Alexa, la télécommande Comcast, etc., la voix et les interactions en langage naturel remplaceront lentement les claviers –<strong> Ram Menon, Avaamo</strong>.
+                        </p>
+
+                        <p><h2><strong>Les supports visuels (intégrés aux appareils d’assistance vocale)</strong></h2>
+
+                        L’Echo Show d’Amazon montre bien comment le support visuel est un élément clé de l’utilisation d’assistants intelligents, mais il ne s’agit que de la partie émergée de l’iceberg. Grâce à une collaboration intelligente entre les fabricants de cette technologie et les fournisseurs de support, de grands progrès pourraient être faits dans ce domaine. Nous devrions voir une plus large gamme de fonctionnalités et de personnalisations en temps réel de supports visuels pour les assistants vocaux en 2018 – <strong>Zohar Dayan, Wibbitz</strong>.
+                        </p>
+
+                        <p><h2><strong> L’impression 3D</strong></h2>
+
+                        Les imprimantes 3D on changé les processus de fabrication dans de nombreux secteurs et elles sont de plus en plus présentes. Il existe maintenant des imprimantes 3D faciles d’utilisation et peu encombrantes pouvant être utilisées par des particuliers. Leur prix diminue, ce qui les rend davantage accessibles. Les imprimantes 3D continueront de révolutionner différents secteurs en 2018 – <strong>Alexandro Pando, Xyrupt</strong>.
+                        </p>
+                    </div>
+                    <div class="col-md-10 col-md-offset-1 animate-box">
+                        
+
+                             
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="instagram-entry">
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-1.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-2.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-3.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-4.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-5.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-6.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-7.jpg);">
+                    </a>
+                    <a href="#" class="instagram text-center" style="background-image: url(images/gallery-8.jpg);">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <footer id="colorlib-footer" role="contentinfo">
+            <div class="container">
+                <div class="row row-pb-md">
+                    <div class="col-md-4">
+                        <h2>Navigation</h2>
+                        <p>
+                            <ul class="colorlib-footer-links">
+                                <li><a href="index.php"><i class="icon-check"></i> Acceuil</a></li>
+                                <li><a href="index.php?page=vers_liste_blog"><i class="icon-check"></i> Blog post</a></li>
+                                <li><a href="index.php?page=vers_connexion_user"><i class="icon-check"></i>Connexion/inscription</a></li>
+                                <li><a href="index.php?page=vers_la_connexion_admin"><i class="icon-check"></i> Se connecter à la partie administration</a></li>
+                                
+                            </ul>
+                        </p>
+                    </div>
+                    <div class="col-md-4">
+                        
+                            
+                    </div>
+                    <div class="col-md-4 col-md-push-1">
+                        <h2>Tags</h2>
+                        <p class="tags">
+                            <span><a href="#"><i class="icon-tag"></i> Métier du web</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Robot et nouvelle technologie</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Transhumaniste </a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Futur de l'homme</a></span>
+                            <span><a href="#"><i class="icon-tag"></i> Le web est il au géant du web ?</a></span>
+                            
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <p>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        <small class="block">Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved. This template is made with <i class="icon-heart3" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></small>
+                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+    </div>
+    
+    <!-- jQuery -->
+    <script src="public/js/jquery.min.js"></script>
+    <!-- jQuery Easing -->
+    <script src="public/js/jquery.easing.1.3.js"></script>
+    <!-- Bootstrap -->
+    <script src="public/js/bootstrap.min.js"></script>
+    <!-- Waypoints -->
+    <script src="public/js/jquery.waypoints.min.js"></script>
+    <!-- Flexslider -->
+    <script src="public/js/jquery.flexslider-min.js"></script>
+    <!-- Owl carousel -->
+    <script src="public/js/owl.carousel.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="public/js/jquery.magnific-popup.min.js"></script>
+    <script src="public/js/magnific-popup-options.js"></script>
+    <!-- Main -->
+    <script src="public/js/main.js"></script>
+    <script src="public/js/addform.js"></script>
+
+    </body>
+</html>
+
