e58b38d488bdf6abdc5221ecdae8f3c6467ac34e
Davidjeje <david.marivat-larose@orange.fr>
2018-09-03 15:29:22 +0200

    adding the vers_admin_message_ajout_blog.php file

---
+++ controleur/vers_admin_message_ajout_blog.php
@@ -0,0 +1,35 @@
+<?php
+function valide_ajout_blog()
+{
+	if (isset($_FILES['image']) AND $_FILES['image']['error'] == 0)
+	{
+        // Testons si le fichier n'est pas trop gros
+        if ($_FILES['image']['size'] <= 2097152)
+        {
+                // Testons si l'extension est autorisée
+                $infosfichier = pathinfo($_FILES['image']['name']);
+                $extension_upload = $infosfichier['extension'];
+                $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+                $image= basename($_FILES['image']['name']);
+                if (in_array($extension_upload, $extensions_autorisees))
+                {
+                        // On peut valider le fichier et le stocker définitivement
+                        $resultat= move_uploaded_file($_FILES['image']['tmp_name'], 'public/images/' . $image);
+                        $dat= new datetime();
+
+                        if ($resultat)
+                        {
+                                include(dirname(__FILE__).'/../modele/requete_sql.php');
+                                $inserer= inserer_le_blog_post();
+                                
+                                include(dirname(__FILE__).'/../admin/validation_blog_ajouter.php');
+                        }
+                        
+                        
+                }
+        }
+	}
+
+	
+}
+?>
