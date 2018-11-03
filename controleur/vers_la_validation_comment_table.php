f8dc69ecbc43bfb6993bf0c7a13750a6d89b46c8
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:35:15 +0100

    Update the file
    controller change

--- controleur/vers_la_validation_comment_table.php
+++ controleur/vers_la_validation_comment_table.php
@@ -1,9 +1,26 @@
 <?php
 function inserer_comment_table()
 {
-	include(dirname(__FILE__).'/../modele/requete_sql.php');
+	/*include(dirname(__FILE__).'/../modele/requete_sql.php');
 	$inserer= inserer_le_commentaire_valider();
 	$sup= supprimer_le_commentaire();
+	include(dirname(__FILE__).'/../admin/comment_definitivement_valider.php');*/
+	function chargerClasse($classname)
+			{
+  				require  'modele/' .$classname.'.php';
+			}
+
+
+			spl_autoload_register('chargerClasse');
+	$Comment= new Comment();
+	$Comment->setId($_GET['Id']),
+			->setBlogPostId($_GET['blogPostId']),
+            ->setAuteur($_GET['auteur']),
+            ->setMessage($_GET['message']);
+            
+            
+	$CommentManager= new CommentManager();
+	$createc= $CommentManager->createc()
 	include(dirname(__FILE__).'/../admin/comment_definitivement_valider.php');
 
 };
