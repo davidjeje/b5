b32e7a6a1cb39843bafffcfbcffd7b68306246ae
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:25:24 +0100

    Update
    controller change

--- controleur/vers_la_validation_comment.php
+++ controleur/vers_la_validation_comment.php
@@ -1,13 +1,20 @@
 <?php
 function afficher_la_validation_comment()
 {
+	/*include(dirname(__FILE__).'/../modele/requete_sql.php');
+	$admin_comment= afficher_le_commentaire_partie_admin();
+	include(dirname(__FILE__).'/../admin/partie_admin_comment.php');*/
 	
-		{	
-			include(dirname(__FILE__).'/../modele/requete_sql.php');
-			$admin_comment= afficher_le_commentaire_partie_admin();
-			include(dirname(__FILE__).'/../admin/partie_admin_comment.php');
-		}
+	function chargerClasse($classname)
+	{
+  		require  'CommentManager/' .$classname.'.php';
+	}
+	spl_autoload_register('chargerClasse');
+	$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+	$CommentManager= new CommentManager($bdd);
+	$read= $CommentManager->readAll1();
 	
+	include(dirname(__FILE__).'/../admin/partie_admin_comment.php');
 };
 
 
