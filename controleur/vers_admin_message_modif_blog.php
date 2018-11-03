c30941e1fc9015978d86d0a6913396e87ed2e199
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:45:23 +0100

    Update the file
    controller change

--- controleur/vers_admin_message_modif_blog.php
+++ controleur/vers_admin_message_modif_blog.php
@@ -9,8 +9,22 @@ function valide_modif_blog()
 	// 2 : Le nombre doit être compris entre 1 et 100
 		if ($_GET['id'] >= 1 AND $_GET['id'] <= 100) 
 		{	
-			include(dirname(__FILE__).'/../modele/requete_sql.php');
+			/*include(dirname(__FILE__).'/../modele/requete_sql.php');
 			$ne= afficher_le_post();
+			include(dirname(__FILE__).'/../admin/validation_blog_modif.php');*/
+			//include(dirname(__FILE__).'/../modele/BlogPostManager.php');
+			function chargerClasse($classname)
+			{
+  				require 'modele/' . $classe . '.php';
+			}
+			spl_autoload_register('chargerClasse');
+			
+			$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+			$BlogPostManager= new BlogPostManager($bdd);
+			$id= $_GET['id'];
+
+			$read= $BlogPostManager->read($id);
+			
 			include(dirname(__FILE__).'/../admin/validation_blog_modif.php');
 		}
 	}
