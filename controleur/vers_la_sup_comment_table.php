f078f25a0d07b1c03f8c6a4319931e2630dceddd
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:14:35 +0100

    Update the file
    controller change

--- controleur/vers_la_sup_comment_table.php
+++ controleur/vers_la_sup_comment_table.php
@@ -9,9 +9,20 @@ function sup_comment_table()
 	// 2 : Le nombre doit être compris entre 1 et 100
 		if ($_GET['id'] >= 1 AND $_GET['id'] <= 100) 
 		{	
-			include(dirname(__FILE__).'/../modele/requete_sql.php');
+			/*include(dirname(__FILE__).'/../modele/requete_sql.php');
 			$sup= supprimer_le_commentaire();
-			include(dirname(__FILE__).'/../admin/comment_definitivement_supprimer.php');
+			include(dirname(__FILE__).'/../admin/comment_definitivement_supprimer.php');*/
+			function chargerClasse($classname)
+			{
+  				require  'CommentManager/' .$classname.'.php';
+			}
+			spl_autoload_register('chargerClasse');
+			$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+			$CommentManager= new CommentManager($bdd);
+			
+			$comment= $CommentManager->read($_GET['id']);
+			$delete= $CommentManager->delete($comment);
+			include(dirname(__FILE__).'/../admin/comment_definitivement_supprimer.php')
 
 		}
 	}
