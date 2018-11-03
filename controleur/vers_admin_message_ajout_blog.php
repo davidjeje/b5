2d22b2433e382ab50737aaa979b7975dd9b9456a
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:43:26 +0100

    Update the file
    controller change

--- controleur/vers_admin_message_ajout_blog.php
+++ controleur/vers_admin_message_ajout_blog.php
@@ -17,10 +17,26 @@ function valide_ajout_blog()
                         $resultat= move_uploaded_file($_FILES['image']['tmp_name'], 'public/images/' . $image);
                         $dat= new datetime();
 
-                        if ($resultat)
+                       // if ($resultat)
                         {
-                                include(dirname(__FILE__).'/../modele/requete_sql.php');
-                                $inserer= inserer_le_blog_post();
+                                function chargerClasse($classname)
+                                {
+                                        require 'modele/' . $classe . '.php';
+                                }
+                                spl_autoload_register('chargerClasse');
+                                
+                                //$inserer= inserer_le_blog_post();                    
+                                $BlogPost= new BlogPost();
+                                $BlogPost->setAuthor($_POST['author']);
+                                $BlogPost->setTitle($_POST['title']);
+                                $BlogPost->setChapo($_POST['chapo']);
+                                $BlogPost->setContent($_POST['content']);
+                                //$BlogPost->setImage($_POST['image']);
+                                $BlogPost->setDateDisplay($_POST['change_date']);
+                                $bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+                                $BlogPostManager = new BlogPostManager($bdd);
+                                $create= $BlogPostManager->create($BlogPost);
+
                                 
                                 include(dirname(__FILE__).'/../admin/validation_blog_ajouter.php');
                         }
