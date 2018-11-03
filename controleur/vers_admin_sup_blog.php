2bc0db220531bda58b4a7ae03fd5ba8ce837b4cd
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:57:16 +0100

    Update the file
    controller change

--- controleur/vers_admin_sup_blog.php
+++ controleur/vers_admin_sup_blog.php
@@ -1,8 +1,21 @@
 <?php
 function afficher_sup_blog()
 {
-	include(dirname(__FILE__).'/../modele/requete_sql.php');
+	/*include(dirname(__FILE__).'/../modele/requete_sql.php');
 	$news= afficher_liste_post();
+	include(dirname(__FILE__).'/../admin/partie_admin_sup.php');*/
+	function chargerClasse($classname)
+	{
+  		require 'modele/' . $classe . '.php';
+	}
+
+
+	spl_autoload_register('chargerClasse');
+	$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+
+	$BlogPostManager = new BlogPostManager($bdd);
+	$readAll= $BlogPostManager->readAll();
+	
 	include(dirname(__FILE__).'/../admin/partie_admin_sup.php');
 }
 ?>
