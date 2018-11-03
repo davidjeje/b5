2a1067505801599a59c469adbc558d568bd6a001
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 03:51:17 +0100

    Update the file
    controller change

--- controleur/vers_liste_blog.php
+++ controleur/vers_liste_blog.php
@@ -1,13 +1,20 @@
 <?php
+	
 function afficher_blog_post()
 {
 	
-	include(dirname(__FILE__).'/../modele/requete_sql.php');
-	$news = afficher_liste_post();
-	$new = afficher_liste_post_recent();
-	include(dirname(__FILE__).'/../vue/liste_blog.php');
+	function chargerMaClasse($classe) 
+	{
+    	require 'modele/' . $classe . '.php';
+	}
+		spl_autoload_register('chargerMaClasse');
+
+	$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+	$BlogPostManager = new BlogPostManager($bdd);//il ne trouve pas la class
+	$readAll= $BlogPostManager->readAll();
+	$readAll1= $BlogPostManager->readAll1();
+	 
+
+	include(dirname(__FILE__).'/../vue/liste_blog.php');	
 	
 }
-
-
-?>
