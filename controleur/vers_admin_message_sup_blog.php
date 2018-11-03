1c2046687bc8eeafa1066e5d4e7251ec4bea8955
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-03 04:48:42 +0100

    Update
    controller change

--- controleur/vers_admin_message_sup_blog.php
+++ controleur/vers_admin_message_sup_blog.php
@@ -9,8 +9,24 @@ function valide_sup_blog()
 	// 2 : Le nombre doit être compris entre 1 et 100
 		if ($_GET['id'] >= 1 AND $_GET['id'] <= 100) 
 		{	
-			include(dirname(__FILE__).'/../modele/requete_sql.php');
-			$sup = supprimer_le_blog_post();
+			function chargerClasse($classname)
+			{
+  				require  'modele/' .$classname.'.php';
+			}
+
+
+			spl_autoload_register('chargerClasse');
+			//$sup = supprimer_le_blog_post();
+			$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
+			$BlogPostManager = new BlogPostManager($bdd);
+			$ok= $_GET['id'];
+			$blog= $BlogPostManager->read($ok);
+
+			$delete = $BlogPostManager->delete($blog);
+			
+
+
+
 			include(dirname(__FILE__).'/../admin/validation_blog_sup.php');
 		}
 	}
