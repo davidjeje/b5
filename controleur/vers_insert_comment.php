74a14b5dc7afe622590e2dbdc9a06b242b3a0a33
davidjeje <38698702+davidjeje@users.noreply.github.com>
2018-11-04 14:49:22 +0100

    Update the file
    change of path manager how

--- controleur/vers_insert_comment.php
+++ controleur/vers_insert_comment.php
@@ -12,7 +12,7 @@ function inserer_comment()
 			
 			function chargerClasse($Comment)
 			{
-  				require  'CommentManager/' .$Comment.'.php';
+  				require  'modele/' .$Comment.'.php';
 			}
 			spl_autoload_register('chargerClasse');
 			$bdd = new PDO('mysql:host=localhost;dbname=blo;charset=utf8', 'root', '');
