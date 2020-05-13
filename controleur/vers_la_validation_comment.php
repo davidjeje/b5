87436bda170140e6474199f07261d0a1d14e7f3c
Davidjeje <david.marivat-larose@orange.fr>
2020-05-13 04:29:30 +0200

    In the commentMananger file, I did the indentation, changing the name of two attributes and three variables located in the function parameters of this file.

--- controleur/vers_la_validation_comment.php
+++ controleur/vers_la_validation_comment.php
@@ -1,5 +1,5 @@
 <?php
-function afficher_la_validation_comment()
+function afficher_la_validation_comment() 
 {
 	/*include(dirname(__FILE__).'/../modele/requete_sql.php');
 	$admin_comment= afficher_le_commentaire_partie_admin();
